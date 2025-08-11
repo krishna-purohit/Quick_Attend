3e0c1a50f1951bc39f56820c4cbf1d6750306d9a
krishna-purohit <shineeroyals@gmail.com>
2025-08-11 21:30:41 +0530

    new changes

--- login/qr.js
+++ login/qr.js
@@ -8,24 +8,3 @@ window.addEventListener("DOMContentLoaded", () => {
     login.classList.add("show");
   }, 3000); // Waits until animations complete
 });
-
-  const form = document.querySelector(".generate-form");
-  const qrPreview = document.getElementById("qrPreview");
-  const qrImage = document.getElementById("qrImage");
-  const downloadBtn = document.getElementById("downloadBtn");
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    // Simulate QR generation — replace with real backend URL later
-    const dummyQR = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=QuickAttend";
-
-    // Show QR
-    qrImage.src = dummyQR;
-    downloadBtn.href = dummyQR;
-    qrPreview.style.display = "block";
-
-    // Optionally hide form
-    form.style.display = "none";
-  });
-
